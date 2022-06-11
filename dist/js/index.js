8e3edc459b8c5b1a8d8805fb9fff93bf08bb15a3
Priscila Melo <melo.priscilatais@gmail.com>
2022-06-11 10:28:16 -0300

    first commit

---
+++ dist/js/index.js
@@ -0,0 +1,32 @@
+"use strict";
+const button = document.getElementById("btn-login");
+const login = document.getElementById("email-login");
+const password = document.getElementById("senha-login");
+const getDataBase = () => { var _a; return (_a = JSON.parse(localStorage.getItem("db_user"))) !== null && _a !== void 0 ? _a : []; };
+const readUserInDB = () => getDataBase();
+const clearValues = () => {
+    const inputs = document.querySelectorAll(".input-login");
+    inputs.forEach((inputs) => (inputs.value = ""));
+};
+const userLoginValidation = (login, password) => {
+    if (localStorage.getItem("db_user") != null) {
+        const dbUser = readUserInDB();
+        const getLogin = dbUser.map((login) => login.login).join("");
+        const getPass = dbUser.map((pass) => pass.pass).join("");
+        if (login == getLogin && password == getPass) {
+            alert("Logado com sucesso!");
+            window.location.href = "recados.html";
+        }
+        else {
+            alert("Login ou senha inválidos!");
+        }
+    }
+    else {
+        alert("Encontramos um erro: NÃO EXISTEM CONTAS REGISTRADAS NESSE LOGIN E SENHA!");
+    }
+};
+button.addEventListener("click", (event) => {
+    userLoginValidation(login.value, password.value);
+    clearValues();
+    event.preventDefault();
+});
